2a4600707e842c6d3728928274b66cf06a8861b8
pwlong <paul@thelongs.ws>
2015-02-10 12:21:47 -0800

    Initial ip_repo file structure and templates generated by Vivado ip packager tool

---
+++ FreqCounter_1.0/drivers/FreqCounter_v1_0/src/FreqCounter.c
@@ -0,0 +1,16 @@
+
+
+/***************************** Include Files *******************************/
+#include "FreqCounter.h"
+
+/************************** Function Definitions ***************************/
+/* PWL this is where we should write the drivers
+ * according to Roy's Project 2 specification we need functions to:
+ *		Initialize the peripheral to the correct mode (we probably don't need this)
+ *		Capture a frequency reading
+ *		Convert count reading to scaled voltage
+ *			corresponding to the average voltage
+ *			applied to the LED
+ * 		Adjust scaling factors based on actual operation
+ * 			of our circuit (characterize the circuit)
+ */ 
