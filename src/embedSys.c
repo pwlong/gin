b17699ec1722348d56cb702c6dadff07a2819f6a
jfluth <jfluth@gmail.com>
2015-02-11 17:19:50 -0800

    added more code from last assignment...

--- src/embedSys.c
+++ src/embedSys.c
@@ -35,6 +35,7 @@
 int debugen = 0;		
 
 /***************** Function Prototypes ***************************************/
+int do_init(void);
 int readVal(void);
 void delay_msecs(unsigned int);
 void update_lcd(int, int, u32);
@@ -47,9 +48,232 @@ volatile unsigned long	timestamp;			// timestamp since the program began
 
 
 int main(void){
+	XStatus 	status;
+	u16			sw, oldSw =0xFFFF;		// 0xFFFF is invalid - makes sure the PWM freq is updated the first time
+	int			rotcnt, oldRotcnt = 0x1000;	
+	bool		done = false;
+	
+	volatile u32 HW_highT, HW_lowT;
+	u32 HW_Frequency, HW_DutyCycle;
+	u32 SW_Frequency, SW_DutyCycle;
+
+	init_platform();
+
+	// initialize devices and set up interrupts, etc.
+ 	status = do_init();
+ 	if (status != XST_SUCCESS) { 
+ 		PMDIO_LCD_setcursor(1,0);
+ 		PMDIO_LCD_wrstring("****** ERROR *******");
+ 		PMDIO_LCD_setcursor(2,0);
+ 		PMDIO_LCD_wrstring("INIT FAILED- EXITING");
+ 		exit(XST_FAILURE);
+ 	}
+ 	
+	// initialize the global variables
+	timestamp = 0;							
+	pwm_freq = INITIAL_FREQUENCY;
+	pwm_duty = INITIAL_DUTY_CYCLE;
+	clkfit = 0;
+	new_perduty = false;
+	inHardware = false;
+    
+	// start the PWM timer and kick of the processing by enabling the Microblaze interrupt
+	PWM_SetParams(&PWMTimerInst, pwm_freq, pwm_duty);	
+	PWM_Start(&PWMTimerInst);
+    microblaze_enable_interrupts();
+    
+	// display the greeting   
+    PMDIO_LCD_setcursor(1,0);
+    PMDIO_LCD_wrstring(" PROJECT TWO CLC");
+	PMDIO_LCD_setcursor(2,0);
+	PMDIO_LCD_wrstring("  Jordan & Paul");
+	NX4IO_setLEDs(0x0000FFFF);
+	delay_msecs(2000);
+	NX4IO_setLEDs(0x00000000);
+		
+    // write the static text to the display
+    PMDIO_LCD_clrd();
+    PMDIO_LCD_setcursor(1,0);
+    PMDIO_LCD_wrstring("FR:      DCY:   ");
+    PMDIO_LCD_setcursor(2,0);
+    PMDIO_LCD_wrstring("FR:      DCY:   ");
+
+    // turn off the LEDs and clear the seven segment display
+    NX4IO_setLEDs(0x00000000);
+    NX410_SSEG_setAllDigits(SSEGLO, CC_BLANK, CC_BLANK, CC_BLANK, CC_BLANK, DP_NONE);
+    NX410_SSEG_setAllDigits(SSEGHI, CC_BLANK, CC_BLANK, CC_BLANK, CC_BLANK, DP_NONE);
+      
+    // main loop
+	do { 
+		// check rotary encoder pushbutton to see if it's time to quit
+		if (PMDIO_ROT_isBtnPressed()) {
+			done = true;
+		}
+		else {
+			new_perduty = false;
+			sw = NX4IO_getSwitches();
+			if (sw != oldSw) {
+				switch (sw & PWM_FREQ_MSK) {
+					case 0x00:	pwm_freq = PWM_FREQ_10HZ;	break;
+					case 0x01:	pwm_freq = PWM_FREQ_100HZ;	break;
+					case 0x02:	pwm_freq = PWM_FREQ_1KHZ;	break;
+					case 0x03:	pwm_freq = PWM_FREQ_10KHZ;	break;
+					case 0x04:	pwm_freq = PWM_FREQ_50KHZ;	break;
+					case 0x05:	pwm_freq = PWM_FREQ_100KHZ;	break;
+					case 0x06:	pwm_freq = PWM_FREQ_200KHZ;	break;
+					case 0x07:	pwm_freq = PWM_FREQ_500KHZ;	break;
+				}
+
+				oldSw = sw;
+				new_perduty = true;
+				NX4IO_setLEDs(sw); 			// show the switches selected with LEDs
+			}
+		
+			// read rotary count and handle duty cycle changes
+			// limit duty cycle to 1% to 99%
+			PMDIO_ROT_readRotcnt(&rotcnt);
+			if (rotcnt != oldRotcnt) {
+				// show the rotary count in hex on the seven segment display
+				NX4IO_SSEG_putU16Hex(SSEGLO, rotcnt);
+
+				// change the duty cycle
+				pwm_duty = MAX(1, MIN(rotcnt, 99));
+				oldRotcnt = rotcnt;
+				new_perduty = true;
+			}
+
+			// update generated frequency and duty cycle	
+			if (new_perduty) {
+				u32 freq, dutycycle;
+			
+				// set the new PWM parameters - PWM_SetParams stops the timer
+				status = PWM_SetParams(&PWMTimerInst, pwm_freq, pwm_duty);
+
+				if (status == XST_SUCCESS) {
+					PWM_GetParams(&PWMTimerInst, &freq, &dutycycle);
+					update_lcd(freq, dutycycle, 1);
+										
+					PWM_Start(&PWMTimerInst);
+
+				}
+			}
+		}
+
+	} while (!done);
+	
+	// wait until rotary encoder button is released		
+	do {
+		delay_msecs(10);
+	} while (PMDIO_ROT_isBtnPressed());
+
+	// we're done,  say goodbye
+	xil_printf("\nThat's All Folks!\n\n");
+	PMDIO_LCD_wrstring("-- Goodbye --");
+	NX410_SSEG_setAllDigits(SSEGHI, CC_BLANK, CC_B, CC_LCY, CC_E, DP_NONE);
+	NX410_SSEG_setAllDigits(SSEGLO, CC_B, CC_LCY, CC_E, CC_BLANK, DP_NONE);
+	delay_msecs(5000);
+	PMDIO_LCD_clrd();
+	cleanup_platform();
+	exit(0);
 
 
+}
 
+/**************************** HELPER FUNCTIONS ******************************/
+		
+/****************************************************************************/
+/**
+* initialize the system
+* 
+* This function is executed once at start-up and after resets.  It initializes
+* the peripherals and registers the interrupt handler(s)
+*****************************************************************************/
+int do_init(void)
+{
+	int status;				// status from Xilinx Lib calls
+	
+	// initialize the Nexys4IO and Pmod544IO hardware and drivers
+	// rotary encoder is set to increment from 0 by DUTY_CYCLE_CHANGE 
+ 	status = NX4IO_initialize(NX4IO_BASEADDR);
+	if (status != XST_SUCCESS)
+	{
+		return XST_FAILURE;
+	}
+	
+	status = PMDIO_initialize(PMDIO_BASEADDR);
+	if (status != XST_SUCCESS)
+	{
+		return XST_FAILURE;
+	}
+	
+	// successful initialization.  Set the rotary encoder
+	// to increment from 0 by DUTY_CYCLE_CHANGE counts per
+	// rotation.
+ 	PMDIO_ROT_init(DUTY_CYCLE_CHANGE, true);
+	PMDIO_ROT_clear();
+	
+	
+	// initialize the GPIO instance
+	status = XGpio_Initialize(&GPIOInst, GPIO_DEVICE_ID);
+	if (status != XST_SUCCESS)
+	{
+		return XST_FAILURE;
+	}
+	// GPIO channel 1 is an 8-bit input port.  bit[7:1] = reserved, bit[0] = PWM output (for duty cycle calculation)
+	// GPIO channel 2 is an 8-bit output port.  bit[7:1] = reserved, bit[0] = FIT clock
+	XGpio_SetDataDirection(&GPIOInst, GPIO_OUTPUT_CHANNEL, 0xFE);
+
+	// initialize the GPIO instance
+	status = XGpio_Initialize(&GPIOInstTime, GPIO_DEVICE_ID2);
+	if (status != XST_SUCCESS)
+	{
+		return XST_FAILURE;
+	}
+	// Two 32b input GPIO's for the high time and the low time
+	XGpio_SetDataDirection(&GPIOInstTime, GPIO_HIGHT_CHANNEL, 0xFF);
+	XGpio_SetDataDirection(&GPIOInstTime, GPIO_LOWT_CHANNEL, 0xFF);
+
+	// initialize the PWM timer/counter instance but do not start it
+	// do not enable PWM interrupts.  Clock frequency is the AXI clock frequency
+	status = PWM_Initialize(&PWMTimerInst, PWM_TIMER_DEVICE_ID, false, AXI_CLOCK_FREQ_HZ);
+	if (status != XST_SUCCESS)
+	{
+		return XST_FAILURE;
+	}
+	
+	// initialize the interrupt controller
+	status = XIntc_Initialize(&IntrptCtlrInst, INTC_DEVICE_ID);
+    if (status != XST_SUCCESS)
+    {
+       return XST_FAILURE;
+    }
+
+	// connect the fixed interval timer (FIT) handler to the interrupt
+    status = XIntc_Connect(&IntrptCtlrInst, FIT_INTERRUPT_ID,
+                           (XInterruptHandler)FIT_Handler,
+                           (void *)0);
+    if (status != XST_SUCCESS)
+    {
+        return XST_FAILURE;
+    }
+ 
+	// start the interrupt controller such that interrupts are enabled for
+	// all devices that cause interrupts.
+    status = XIntc_Start(&IntrptCtlrInst, XIN_REAL_MODE);
+    if (status != XST_SUCCESS)
+    {
+        return XST_FAILURE;
+    }
+
+	// enable the FIT interrupt
+    XIntc_Enable(&IntrptCtlrInst, FIT_INTERRUPT_ID);
+
+    // set the duty cycles for RGB1.  The channels will be enabled/disabled
+    // in the FIT interrupt handler.  Red and Blue make purple
+    NX4IO_RGBLED_setDutyCycle(RGB1, 64, 0, 64);
+    NX4IO_RGBLED_setChnlEn(RGB1, false, false, false);
+
+	return XST_SUCCESS;
 }
 
 
@@ -63,6 +287,7 @@ float bangBang(float setPoint, float realPoint) {
 	
 	return output;
 }
+
 
 /****************************************************************************
 *
